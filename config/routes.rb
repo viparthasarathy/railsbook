40bfa6d230e7ce1fd5b02355746870f5ef90bf8a
Vidul Parthasarathy <viparthasarathy@gmail.com>
2015-02-09 12:51:45 -0800

    create user page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   get  'home/index' => 'home#index'
   root 'home#index'
+  resources :users
 end
