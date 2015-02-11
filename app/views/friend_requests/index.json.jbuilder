3c2dca50f65db4c88cd5c417237e31442183b28c
Vidul Parthasarathy <viparthasarathy@gmail.com>
2015-02-10 17:01:06 -0800

    refactor user show page

--- app/views/friend_requests/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@friend_requests) do |friend_request|
-  json.extract! friend_request, :id, :user_id, :friend_id, :approved
-  json.url friend_request_url(friend_request, format: :json)
-end
--- test/integration/friending_test.rb
+++ test/integration/friending_test.rb
@@ -15,29 +15,38 @@ class FriendingTest < ActionDispatch::IntegrationTest
     assert_select 'form', count: 0
   end
 
-  test "user should be able to create and destroy friend requests" do
+  test "user should be able to create and cancel friend requests" do
   	create_request
   	login_as(@user)
   	get user_path(@alt_user)
   	assert_select 'form' do
   	  assert_select "[value=?]", "Cancel Request"
   	end
-  	request = @user.friend_requests.first
+  	request = @user.friend_requests.find_by(friend_id: @alt_user.id)
   	assert_difference 'FriendRequest.count', -1 do
   	  delete friend_request_path(request)
   	end
   end
 
-  test "alt user should be able to accept friend requests" do
+  test "alt user should be able to accept friend requests and end friendship" do
     create_request
   	login_as(@alt_user)
   	get user_path(@user)
     assert_select 'form' do
       assert_select "[value=?]", "Accept Request"
     end
-    request = @user.friend_requests.first
+    request = @user.friend_requests.find_by(friend_id: @alt_user.id)
     assert_difference '@alt_user.reload.friends.count', 1 do
       put friend_request_path(request), friend_request: {user_id: 1, friend_id: 2, approved: true }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    get user_path(@user)
+    assert_select 'form' do
+      assert_select "[value=?]", "Remove Friend"
+    end
+    assert_difference '@alt_user.reload.friends.count', -1 do
+      delete friend_request_path(request)
     end
     assert_redirected_to root_url
   end
@@ -49,13 +58,26 @@ class FriendingTest < ActionDispatch::IntegrationTest
   	assert_select 'form' do
   	  assert_select "[value=?]", "Decline Request"
   	end
-  	request = @user.friend_requests.first
+  	request = @user.friend_requests.find_by(friend_id: @alt_user.id)
   	assert_no_difference '@alt_user.reload.friends.count' do
   	  delete friend_request_path(request)
   	end
   	assert_redirected_to root_url
   end
 
+  test "user should be able to remove friendship" do
+    request = FriendRequest.new(user_id: @user.id, friend_id: @alt_user.id, approved: true)
+    request.save
+    login_as(@user)
+    get user_path(@alt_user)
+    assert_select 'form' do
+      assert_select "[value=?]", "Remove Friend"
+    end
+    assert_difference '@user.reload.friends.count', -1 do
+    	delete friend_request_path(request)
+    end
+    assert_redirected_to root_url
+  end
 
   private
 
